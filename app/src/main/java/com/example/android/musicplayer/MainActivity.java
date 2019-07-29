87e99e5e198d79fe424e5d7ce0dfe01f0223b757
GrandCkay <cf.ckay@gmail.com>
2019-07-29 15:57:49 +0300

    Implemented button functionality

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -1,30 +1,28 @@
 package com.example.android.musicplayer;
 
-import androidx.appcompat.app.ActionBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
-import android.content.Context;
-import android.graphics.Color;
 import android.graphics.PorterDuff;
-import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.text.Html;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
-import java.security.AccessController;
-import java.sql.Time;
+
 import java.util.Timer;
 import java.util.TimerTask;
+
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
     ImageButton imageButtonArrow;
     SeekBar seekBar;
+    boolean nextReset = true;
 
 
     @Override
@@ -32,9 +30,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.babek);
-
         imageButtonArrow = findViewById(R.id.imageButtonArrow);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.babek);
 
         seekBar = findViewById(R.id.seekBar);
         seekBar.setMax(mediaPlayer.getDuration());      // устанавливаем максимум равный длительности трека (getDuration - возвращает длительность аудио трека)
@@ -49,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
             }
@@ -57,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
-            });
+        });
 
 
         new Timer().scheduleAtFixedRate(new TimerTask() {  // создаем новый метод класса Тaймер и устанавливаем новое задание (ТаймерТеск)
@@ -69,32 +66,45 @@ public class MainActivity extends AppCompatActivity {
         }, 0, 1000);  // 0 - (задержка) через какое время запустить таймер; 10000 - периодичность
 
 
-    int color = ContextCompat.getColor(getBaseContext(), R.color.myColor);
-    seekBar = findViewById(R.id.seekBar);
-    seekBar.getProgressDrawable().setColorFilter(color, PorterDuff.Mode.SRC_ATOP); // полоска
-    seekBar.getThumb().setColorFilter(color, PorterDuff.Mode.SRC_ATOP); // кругляшок
-
+        int color = ContextCompat.getColor(getBaseContext(), R.color.myColor);
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.getProgressDrawable().setColorFilter(color, PorterDuff.Mode.SRC_ATOP); // полоска
+        seekBar.getThumb().setColorFilter(color, PorterDuff.Mode.SRC_ATOP); // кругляшок
 
     }
 
     public void previous(View view) {
-
-
+        mediaPlayer.stop();
+        seekBar.setMax(mediaPlayer.getDuration());
+        mediaPlayer.prepareAsync();
+        imageButtonArrow.setImageResource(R.drawable.ic_play_arrow_beige_24dp);
+        nextReset = true;
     }
 
     public void arrow(View view) {
-        if (mediaPlayer.isPlaying()) {
-            mediaPlayer.pause();
-            imageButtonArrow.setImageResource(R.drawable.ic_play_arrow_beige_24dp);
-        } else {
-            mediaPlayer.start();
-            imageButtonArrow.setImageResource(R.drawable.ic_pause_beige_24dp);
+
+        TextView nameTrack = findViewById(R.id.textViewName);
+        nameTrack.setText("Бабек Мамедрзаев - Принцесса");
+
+        if (nextReset) {
+
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.pause();
+                imageButtonArrow.setImageResource(R.drawable.ic_play_arrow_beige_24dp);
+            } else if (nextReset) {
+                mediaPlayer.start();
+                imageButtonArrow.setImageResource(R.drawable.ic_pause_beige_24dp);
+            }
         }
     }
 
     public void next(View view) {
-
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            seekBar.setMax(100);
+            imageButtonArrow.setImageResource(R.drawable.ic_stop_beige_24dp);
+            nextReset = false;
         }
-
-
     }
+
+}
